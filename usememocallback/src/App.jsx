9959c4e524d57024186ca10d65f3cb4bff7b3fff
Mohnish <mohnish.a007@gmail.com>
2023-06-26 00:00:06 +0100

    managing the reference value and original value

--- usememocallback/src/App.jsx
+++ usememocallback/src/App.jsx
@@ -4,12 +4,22 @@ import './App.css'
 
 function App() {
   const [number, setNumber] = useState([10,20,30])
-  const  total = useMemo(() => number.reduce((acc, number)=> acc+number,0),[number])
+  const  total = useMemo(
+    () => number.reduce((acc, number)=> acc+number,0),
+    [number])
 
+  const [names, setNames] = useState(['b','c','a','e','d'])  
+  const sortedNames = [...names].sort(); // duplicte copy is been sorted instead of original copy
   return (
     <>
       <div>
         total is {total}
+      </div> <br />
+      <div>
+        Names: {...names.join(',')}   
+      </div> <br />
+      <div>
+        Sorted Names: {sortedNames.join(',')}
       </div>
     </>
   )
