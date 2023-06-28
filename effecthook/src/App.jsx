e847f22298050c2e6efd65fe00cef350ceedb07f
Mohnish <mohnish.a007@gmail.com>
2023-06-28 16:18:03 +0100

    basic fetch is completed

--- effecthook/src/App.jsx
+++ effecthook/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [names, setNames] = useState([])
+  fetch("names.json")
+  .then(response => response.json())
+  .then(data => setNames(data))
 
   return (
     <>
       <div>
-        Hello World!
+       Names are: {names.join(",")}
       </div>
       
     </>
