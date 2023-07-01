372c9712f332392c64551c05b436c44447d1f86c
Mohnish <mohnish.a007@gmail.com>
2023-07-01 23:04:39 +0100

    Error in map functio is resolved - use () brackets instead of {} brackets

--- ref-useRef/src/App.jsx
+++ ref-useRef/src/App.jsx
@@ -5,6 +5,11 @@ import './App.css'
 function App() {
   const inputRef = useRef(null);
   const [names, setNames] = useState([])
+  const [myObj, setMyObj] = useState([
+    {id:1, name: "abc"},
+    {id:2, name: "xyz"}
+  ])
+  const objRef = useRef()
   //console.log("app rerender")
   useEffect(()=> {
     inputRef.current.focus();
@@ -15,18 +20,36 @@ function App() {
       console.log(names)
       //inputRef.current.value = "";
   };
+  const addObj = () => {
+          console.log(myObj)
+  }
+
   return (
     <>
       <div>
         <div>
-        {names.map((name, index)=>{
-          <p key={index}>{index}. Name is {name}</p>
-        })}
+          <ul>
+        {names.map((name, index)=>(
+            
+            <li key={index}>{index}. Name is {name}</li>
+            
+        ))}
+        </ul>
         </div>
         <div>
-        <input type="text" ref={inputRef} onChange={e => setNames(...names,e.target.value)}/>
+        <input type="text" ref={inputRef} />
         <button onClick={onAddName}>Add Name</button>
-        <div>current name is {names}</div>
+        <button onClick={addObj}>Add Obj</button>
+        <div>current name is {names}</div> <br />
+        <div>
+          <ul>
+          {myObj.map((obj) => (
+            
+            <li key={obj.id}>{obj.id}. Name is {obj.name}</li>
+            
+          ))}
+          </ul>
+        </div>
         </div>
       </div>
      
