bbe098bd45f15e5812e268896a7c718f794e4b27
Mohnish <mohnish.a007@gmail.com>
2023-07-01 19:16:50 +0100

    useRef for state mangement is not working

--- ref-useRef/src/App.jsx
+++ ref-useRef/src/App.jsx
@@ -3,16 +3,28 @@ import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const inputRef = useRef(null);
+  const [names, setNames] = useState([])
+  
   useEffect(()=> {
-    inputRef.current.focus()
+    inputRef.current.focus();
   },[]);
-
+  function onAddName() {
+      setNames([...names,inputRef.current.value]);
+      console.log(inputRef.current.value)
+      console.log(names)
+      inputRef.current.value = "";
+  };
   return (
     <>
       <div>
+        <div>
+        {names.map((name)=>{
+          <div key={name}>Name is {name}</div>
+        })}
+        </div>
         <input type="text" ref={inputRef} />
+        <button onClick={onAddName}>Add Name</button>
       </div>
      
     </>
