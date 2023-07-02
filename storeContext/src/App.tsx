18591cdeef0f64f361c56d08ae716637c2be994a
Mohnish <mohnish.a007@gmail.com>
2023-07-02 20:33:03 +0100

    Refactoring of data related code is done

--- storeContext/src/App.tsx
+++ storeContext/src/App.tsx
@@ -1,32 +1,8 @@
 import { useState, useEffect, createContext, useContext } from 'react'
-
+import {usePokemon, usePokemonSource, PokemonContext} from './store'
 import './App.css'
 
-interface Pokemon {
-  id: number;
-  name: string;
-  type: string[];
-  hp: number;
-  attack: number;
-  defense: number;
-  special_attack: number;
-  special_defense: number;
-  speed: number;
-}
 
-// creating custome hook
-function usePokemonSource():{pokemon: Pokemon[]}{
-  const [pokemon, setPokemon] = useState<Pokemon[]>([])
-  useEffect(()=>{
-      fetch("/pokemon.json")
-        .then((response) => response.json())
-        .then((data) => setPokemon(data))
-  }, [])
-  return {pokemon}}
-
-  function usePokemon(){
-    return useContext(PokemonContext)
-  }
   const PokemonList = ():React.ReactElement => {
   const {pokemon} = usePokemon(); // use of context
    return( <div>
@@ -37,10 +13,7 @@ function usePokemonSource():{pokemon: Pokemon[]}{
     </div>
    )
   }
-// Understanding types here is vital
-  const PokemonContext = createContext<ReturnType<typeof usePokemonSource>>(
-    {} as unknown as ReturnType<typeof usePokemonSource>)
-  // handling the undefined part of source code
+
   function App() {
     //const {pokemon} = usePokemon()
   return (
