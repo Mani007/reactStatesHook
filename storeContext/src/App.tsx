bebbe8a20b015a04669aeedc72cefc5ccf6eb157
Mohnish <mohnish.a007@gmail.com>
2023-07-02 23:11:13 +0100

    Search string getter

--- storeContext/src/App.tsx
+++ storeContext/src/App.tsx
@@ -1,7 +1,18 @@
 //import { useState, useEffect, createContext, useContext } from 'react'
 import {usePokemon, PokemonProvider} from './store'
 import './App.css'
-
+function SearchBox() {
+  // const { search, setSearch } = usePokemon();
+  const {search, setSearch} = usePokemon()
+  return (
+    <input
+      className="mt-3 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-800 focus:ring-indigo-800 sm:text-lg p-2"
+      placeholder="Search"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+    />
+  );
+}
 
   const PokemonList = ():React.ReactElement => {
   const {pokemon} = usePokemon(); // use of context
@@ -33,6 +44,7 @@ import './App.css'
       <div>
         <PokemonProvider>
           <div className="mx-auto max-w-3xl">
+            <SearchBox/>
         <PokemonList />
         </div>
         </PokemonProvider>
--- storeContext/src/store.tsx
+++ storeContext/src/store.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, createContext, useContext } from 'react'
+import { useReducer ,useState, useEffect, createContext, useContext, useCallback } from 'react'
 interface Pokemon {
     id: number;
     name: string;
@@ -11,17 +11,54 @@ interface Pokemon {
     speed: number;
   }
 // creating custome hook
- function usePokemonSource():{pokemon: Pokemon[]}{
-    const [pokemon, setPokemon] = useState<Pokemon[]>([])
+ function usePokemonSource():{
+  pokemon: Pokemon[],
+  search: string
+  setSearch: (search: string) => void } {
+    // const [pokemon, setPokemon] = useState<Pokemon[]>([])
+    // const [search, setSearch] = useState<string>("")
+    
+    type PokemonState = {
+      pokemon: Pokemon[],
+      search: string
+    }
+
+    type PokemonActions = 
+    {type: "setPokemon", payload: Pokemon[]} | {type: "setSearch", payload: string}
+    // useReducer hook below
+    const [{pokemon, search}, dispatch] = useReducer((state: PokemonState,action: PokemonActions) => {
+      switch(action.type) {
+        case "setPokemon":
+          return {...state, pokemon: action.payload}
+        case "setSearch":
+            return {...state, search: action.payload}
+      }
+    },{ // Initial state
+      pokemon: [],
+      search: ""
+    })
+
     useEffect(()=>{
         fetch("/pokemon.json")
           .then((response) => response.json())
-          .then((data) => setPokemon(data))
+          .then((data) => dispatch({
+            type: "setPokemon",
+            payload: data}))
     }, [])
-    return {pokemon}}
+
+    const setSearch = useCallback((search: string) => {
+      dispatch({
+        type: "setSearch",
+        payload: search
+      })
+},[])
+
+    return {pokemon, search, setSearch}}
 
  const PokemonContext = createContext<ReturnType<typeof usePokemonSource>>(
         {} as unknown as ReturnType<typeof usePokemonSource>)
+
+
 
 export function usePokemon(){
         return useContext(PokemonContext)
