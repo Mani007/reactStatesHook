2f1fb56b551a2922bc78f95abe99f8ef79c6fb2e
Mohnish <mohnish.a007@gmail.com>
2023-07-02 21:42:32 +0100

    Setup with tailwind css and styling is done

--- storeContext/src/App.tsx
+++ storeContext/src/App.tsx
@@ -5,12 +5,24 @@ import './App.css'
 
   const PokemonList = ():React.ReactElement => {
   const {pokemon} = usePokemon(); // use of context
-   return( <div>
-    {/* <div>The THEME is {theme}</div> */}
-      {pokemon.map((p)=>(
-        <div key={p.id}> {p.name}</div>
+   return( 
+    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-3">
+      {pokemon.map((p) => (
+        <li
+          key={p.id}
+          className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+        >
+          <div className="flex-1 flex flex-col p-8">
+            <img
+              className="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${p.id}.png`}
+              alt=""
+            />
+            <h3 className="mt-6 text-gray-900 text-sm font-medium">{p.name}</h3>
+          </div>
+        </li>
       ))}
-    </div>
+    </ul>
    )
   }
 
@@ -20,7 +32,9 @@ import './App.css'
     <>
       <div>
         <PokemonProvider>
+          <div className="mx-auto max-w-3xl">
         <PokemonList />
+        </div>
         </PokemonProvider>
       </div>
       
